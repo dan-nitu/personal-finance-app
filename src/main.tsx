9e0377c3c14b91247e090bd8f93fb94d7bd3a958
Voodoo30fps <voodoo30fps@gmail.com>
2026-05-27 10:21:21 +0300

    added fonts, did the initial cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import './assets/scss/main.scss'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './assets/scss/main.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
